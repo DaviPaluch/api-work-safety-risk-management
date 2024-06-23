c4f3da83ad32e7cef7a7d73adb2ddb6a0b018f77
davi paluch <davipaluch@icloud.com>
2024-06-23 17:39:15 -0300

    enviando api para a cloud

--- database/backup/backup.sql
+++ database/backup/backup.sql
@@ -17,8 +17,8 @@ INSERT INTO setor (id, label) VALUES
   ('8', 'Vendas e Marketing');
 
 -- pessoa
-INSERT INTO user (nome,email,password_hash) VALUES
-  ('Davi Paluch','davi@email.com','a1234567@')
+INSERT INTO public.user (id,name,email,password_hash) VALUES
+  ('1','Davi Paluch','davi@email.com','a1234567@');
 
 INSERT INTO estrategia_resolucao (id, label) VALUES
 ('11111111-1111-1111-1111-111111111111', 'diversificação'),
@@ -26,10 +26,8 @@ INSERT INTO estrategia_resolucao (id, label) VALUES
 ('33333333-3333-3333-3333-333333333333', 'seguro'),
 ('44444444-4444-4444-4444-444444444444', 'transferência de risco'),
 ('55555555-5555-5555-5555-555555555555', 'estratégia defensiva'),
-('66666666-6666-6666-6666-666666666666', 'gestão ativa'),
+('66666666-6666-6666-6666-666666666666', 'gestão ativa'),  
 ('77777777-7777-7777-7777-777777777777', 'reserva de emergência'),
 ('88888888-8888-8888-8888-888888888888', 'análise de risco'),
 ('99999999-9999-9999-9999-999999999999', 'controle de alavancagem'),
 ('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'modelagem de cenários');
-
-
