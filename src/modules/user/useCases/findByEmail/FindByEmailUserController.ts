983ba349b5b6c545ac75987ff9eb6f5156e09ccd
davi paluch <davipaluch@icloud.com>
2024-06-28 10:48:01 -0300

    subindo alteracoes na nuvem

--- src/modules/user/useCases/findByEmail/FindByEmailUserController.ts
+++ src/modules/user/useCases/findByEmail/FindByEmailUserController.ts
@@ -6,10 +6,10 @@ import { Request, Response } from "express"
 
 class FindByEmailUserCrontroller {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { email } = req.body
+    const { email } = req.query
     try {
       const findByEmailUserUseCase = container.resolve(FindByEmailUserUseCase)
-      const exist = await findByEmailUserUseCase.execute({ email })
+      const exist = await findByEmailUserUseCase.execute({ email: String(email) })
       return res.status(201).json({ status: "200", emailCadastrado: exist });
     } catch (err) {
       return res.status(401).send(err.message);
